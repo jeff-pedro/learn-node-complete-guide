d536b04dd8deaadb05393f50dce4dd1e6e3a62cd
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-15 18:33:12 -0300

    parsing request bodies

--- app.js
+++ app.js
@@ -14,8 +14,17 @@ const server = http.createServer((req, res) => {
     }
 
     if (url === '/message' && method === 'POST') {
-        let message = req.body; 
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = []; 
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
