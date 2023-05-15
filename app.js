991c8f688bb6b024c3041d8b361cd07ab03d892e
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-15 16:03:18 -0300

    sending responses

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
