02fd081192b767dc5ee53bc16291a8d7a3b6c2cb
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-15 16:21:51 -0300

    routing requests

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
