6d66c89a01e47bf0d0db60c837d61a6b360bd83a
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-15 19:27:24 -0300

    refactoring to non-blocking code

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
         });
 
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, () => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         })
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
