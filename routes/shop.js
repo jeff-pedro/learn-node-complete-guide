ea47e94edafc4af3160055ed50b57bc1f3a6abb3
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-17 18:49:42 -0300

    serve html pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
