cdccb3c64cbf813f324078d8666db27f58f8e8e4
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-22 15:31:47 -0300

    working on the layout with ejs partials

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,9 @@ router.get('/', (req, res, next) => {
     products: products,
     pageTitle: 'Shop',
     path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
