58311ddc9894c6404e830bab3f69efbb526c1505
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-23 21:36:35 -0300

    storing product data

--- models/product.js
+++ models/product.js
@@ -18,8 +18,11 @@ const getProductFromFile = (cb) => {
 }
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   save() {
