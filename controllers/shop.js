4aae6358ad1a91019bb53a653dbd29d7cd75d782
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-28 17:28:45 -0300

    displaying Cart items on the Cart page

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,7 +33,23 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', { pageTitle: 'Your Cart', path: '/cart' })
+  Product.fetchAll((products) => {
+    Cart.getCart((cart) => {
+      const cartProducts = [];
+      for (let product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id)
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+        products: cartProducts
+      });
+    });
+  });
+
 }
 
 exports.postCart = (req, res, next) => {
