4b5a17b516dce7fc12715cf7ed483dd5860f9b5d
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-06-17 19:43:53 -0300

    insert data and create a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -14,14 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(null, title, imageUrl, price, description);
-
-  product
-  .save()
-  .then(() => {
-    res.redirect('/');
-  })
-  .catch(err => console.log(err));
+  Product.create({ title, price, imageUrl, description })
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getEditProduct = (req, res, next) => {
