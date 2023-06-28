fbe6687d5066e2b3d6d8f5e96f5465bf78930976
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-06-28 19:02:49 -0300

    add an order model

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,6 +43,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
@@ -52,7 +57,6 @@ sequelize
     if (!user) {
       return User.create({ name: 'Jeff', email: 'test@test.com' });
     }
-    // return Promise.resolve(user);
     return user;
   })
   .then(user => {
