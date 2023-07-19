191edc3509f205d84e210b61303dc1d0463d17b0
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-07-19 17:55:40 -0300

    create class to Product model

--- models/product.js
+++ models/product.js
@@ -1,26 +1,13 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description= description;
   }
-});
+
+  save(){};
+};
 
 module.exports = Product;
